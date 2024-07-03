24e60fef39493a461fbe124084dca3788802e13e
cl9wnn <egor_udir@mail.ru>
2024-07-03 11:59:35 +0300

    pu

--- GallowsGame/GallowsGame/MainPage.xaml.cs
+++ GallowsGame/GallowsGame/MainPage.xaml.cs
@@ -11,6 +11,7 @@
         private void OnExitButtonClicked(object sender, EventArgs e)
         {
             Environment.Exit(0);
+            Console.WriteLine("gwegwg");
         }
 
         private void OnStartGameButtonClicked(object sender, EventArgs e)
